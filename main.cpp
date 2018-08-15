38b4ab053e387f22eaef3f59307277fa2e4c013c
Jotelly Barros <jotelly@gmail.com>
2018-08-15 15:35:56 -0300

    Using Vector whit Iterator

--- main.cpp
+++ main.cpp
@@ -3,46 +3,14 @@
 
 int main()
 {
-    //Definição das linhas e colunas
-    size_t no_of_rows = 0, no_of_col = 0;
-    int initial_value = 0;
+    //Using Vector whit Iterator
+    std::vector<int> values={1,2,3,4,5,6,7,8,9,10};
+    std::vector<int>::iterator it;
 
-    //Declaração do vetor de vetores
-    std::vector < std::vector < int > > matrix(no_of_rows, std::vector< int >(no_of_col,initial_value));
-
-    //Inicialização de um vetor row para inserção.
-    std::vector<int> row;
-
-    //Add elemento a row
-    row.push_back(2);
-    row.push_back(5);
-    row.push_back(7);
-    matrix.push_back(row);
-
-    //Apaga elementos da row
-    row.clear();
-
-    //Add elemento a row
-    row.push_back(3);
-    row.push_back(6);
-    row.push_back(8);
-    matrix.push_back(row);
-
-    //Apaga elementos da row
-    row.clear();
-
-    //Add elemento a row
-    row.push_back(4);
-    row.push_back(7);
-    row.push_back(9);
-    matrix.push_back(row);
-
-    for ( const std::vector<int> &v : matrix ){
-        for ( int x : v ) {
-            std::cout << '[' << x << ']';
-        }
-        std::cout << std::endl;
-    }
+    it=values.begin();
+    std::advance(it,2);
+    std::cout << *it << std::endl;
+    std::cout << *next(it,1) << std::endl;
 
     return 0;
 }
