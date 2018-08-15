7da88d79026d291a5659656206aa84db496870ea
Jotelly Barros <jotelly@gmail.com>
2018-08-15 15:27:51 -0300

    Utilizando STL vector (2d)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 int main()
 {
-    std::vector<int> myvector_1d;
-    size_t i = 0;
+    //Definição das linhas e colunas
+    size_t no_of_rows = 0, no_of_col = 0;
+    int initial_value = 0;
 
-    myvector_1d.push_back(2);
-    myvector_1d.push_back(4);
-    myvector_1d.push_back(6);
-    myvector_1d.push_back(8);
+    //Declaração do vetor de vetores
+    std::vector < std::vector < int > > matrix(no_of_rows, std::vector< int >(no_of_col,initial_value));
 
-    while (!myvector_1d.empty()) {
-        std::cout << "Valor do vetor 1D [" << i << "] = " << myvector_1d[i] << std::endl;
-        i++;
-        myvector_1d.pop_back();
+    //Inicialização de um vetor row para inserção.
+    std::vector<int> row;
+
+    //Add elemento a row
+    row.push_back(2);
+    row.push_back(5);
+    row.push_back(7);
+    matrix.push_back(row);
+
+    //Apaga elementos da row
+    row.clear();
+
+    //Add elemento a row
+    row.push_back(3);
+    row.push_back(6);
+    row.push_back(8);
+    matrix.push_back(row);
+
+    //Apaga elementos da row
+    row.clear();
+
+    //Add elemento a row
+    row.push_back(4);
+    row.push_back(7);
+    row.push_back(9);
+    matrix.push_back(row);
+
+    for ( const std::vector<int> &v : matrix ){
+        for ( int x : v ) {
+            std::cout << '[' << x << ']';
+        }
+        std::cout << std::endl;
     }
 
     return 0;
