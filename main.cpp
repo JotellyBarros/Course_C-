0d013ca0f5f0b1e5e72fd28df48a4bda463d2068
Jotelly Barros <jotelly@gmail.com>
2018-08-15 15:03:05 -0300

    Vetor 1D

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
-using namespace std;
+#include <vector>
+#include <map>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::vector<int> myvector_1d;
+    size_t i = 0;
+
+    myvector_1d.push_back(2);
+    myvector_1d.push_back(4);
+    myvector_1d.push_back(6);
+    myvector_1d.push_back(8);
+
+    while (!myvector_1d.empty()) {
+        std::cout << "Valor do vetor 1D [" << i << "] = " << myvector_1d[i] << std::endl;
+        i++;
+        myvector_1d.pop_back();
+    }
+
     return 0;
 }
