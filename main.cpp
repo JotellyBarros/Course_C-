e4c3ffc5b29c5b48df60856b4dc416e9876388eb
Jotelly Barros <jotelly@gmail.com>
2018-08-16 10:07:10 -0300

    Using list whit find

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <algorithm> // to using find
 #include <list>
 
 int main()
 {
-    // Using list
+    // Using list whit find
     // Create a list containing integers
-    std::list<int> somelist;
+    std::list<int> someList;
+
+    int numberFound = 4;
 
     // Add an integer to the beginning of the list
-    somelist.push_back(1);
-    somelist.push_back(2);
-    somelist.push_back(3);
+    someList.push_back(1);
+    someList.push_back(2);
+    someList.push_back(3);
 
-    std::cout << somelist.front() << std::endl;
-    somelist.pop_front();
-    std::cout << somelist.front() << std::endl;
-    somelist.pop_front();
-    std::cout << somelist.front() << std::endl;
+    // Using find
+    auto findNow = std::find(someList.begin(), someList.end(), numberFound);
+
+    if (findNow != std::end(someList)) {
+        std::cout << "Number found" << std::endl;
+    } else {
+        std::cout << "Number not found!" << std::endl;
+    }
 
     return 0;
 }
