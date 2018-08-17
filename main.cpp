3b5a2d5c9bd228523af7069a9f7ddabed020f1cd
Jotelly Barros <jotelly@gmail.com>
2018-08-17 09:35:33 -0300

    Use String

--- main.cpp
+++ main.cpp
@@ -1,36 +1,47 @@
 #include <iostream>
-#include <set> // to using set
+#include <string>
 
 int main()
 {
-    // Using set whit iterator
-    // Create a set containing integers
-    std::set<int> setOfNumbers;
+    // Using string
+    // Declaring of variables
+    std::string str_nome;
+    std::string str_sobrenome;
+    std::string str_nome_sobrenome;
 
-    // Inserting four elements
-    setOfNumbers.insert(0);
-    setOfNumbers.insert(1);
-    setOfNumbers.insert(2);
-    setOfNumbers.insert(3);
+    // Coping a new value
+    str_nome = "Jotelly";
+    str_sobrenome = "Barros";
 
-    // Clear the set
-    // setOfNumbers.clear();
+    // Concatenates str_nome whit str_sobrenome
+    str_nome_sobrenome = str_nome + " " + str_sobrenome;
 
-    // Erase a element of set
-    // setOfNumbers.erase(2);
+    // Printing the str_nome_sobrenome
+    std::cout << str_nome_sobrenome << std::endl;
 
-    // Printing of size set
-    std::cout << "Set Size = " << setOfNumbers.size() << " Elements." << std::endl;
+    // Size total of str_nome_sobrenome
+    std::cout << str_nome_sobrenome.size() << " Carácter" << std::endl;
 
-    // Printig all elements of set using iterator
-    std::set<int>::iterator it = setOfNumbers.begin();
+    // Return the position of value passed
+    size_t found = str_nome_sobrenome.find("o");
+    //std::cout << found << std::endl;
 
-    // std::advance(it,1);
-    // std::cout << *it << std::endl;
+    // Return the first position of value passed
+    std::string search = "lly";
+    found = str_nome_sobrenome.find(search);
 
-    for (setOfNumbers.begin(); it != setOfNumbers.end(); ++it) {
-        std::cout << *it << std::endl;
-    }
+    // Use of replacing for change the variable 'o'
+    str_nome_sobrenome.replace(1,1,"ó"); //size_t pos, size_t len, const string
+    std::cout << str_nome_sobrenome << std::endl;
+
+    // Use of replacing for change the word 'Barros' -> 'Oliveira'
+    str_nome_sobrenome.replace(9,6,"Oliveira"); //size_t pos, size_t len, const string
+    std::cout << str_nome_sobrenome << std::endl;
+
+//    search 3; found 4;
+//    for (found; found-search.size() <= search.size(); ++found) {
+//        std::cout << str_nome_sobrenome[found] << std::endl;
+//    }
 
     return 0;
 }
