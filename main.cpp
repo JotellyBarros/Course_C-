9008afbecbc06ebaddfb087e1026528adbb65b93
Jotelly Barros <jotelly@gmail.com>
2018-08-17 11:30:01 -0300

    Use Class POO

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
-// C++ program to demostrate Using struct
+// C++ program to demostrate Using Class POO
 #include <iostream>
 
-struct DateStruct
-{
-    int year;
-    int month;
-    int day;
+//struct DateStruct
+//{
+//    int year;
+//    int month;
+//    int day;
 
-    // Function for printing the values
+//    // Function for printing the values
+//    void print(std::string nameDay){
+//        std::cout << nameDay << " is: " << year << "/" << month << "/" << day << std::endl;
+//    }
+//};
+
+class DateClass
+{
+public:
+    int m_year;
+    int m_month;
+    int m_day;
+
+    // Print employee information to the screen
     void print(std::string nameDay){
-        std::cout << nameDay << " is: " << year << "/" << month << "/" << day << std::endl;
+        std::cout << nameDay << " is: " << m_year << "/" << m_month << "/" << m_day << std::endl;
     }
 };
 
 int main()
 {
     // Initialization of struct
-    DateStruct yesteday {2018,8,16};
-    DateStruct today {2018,8,17};
-    DateStruct tomorrow {2018,8,18};
+    DateClass yesteday {2018,8,16};
+    DateClass today {2018,8,17};
+    DateClass tomorrow {2018,8,18};
 
-    //today.day = 5; // use member selection operator to select a member of the struct
+    //today.m_day = 5; // use member selection operator to select a member of the struct
     yesteday.print("Yesterday");
     today.print("Today");
     tomorrow.print("Tomorrow");
