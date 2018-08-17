f40f85efc395a0b82dc93f4ce76d722579c1d32d
Jotelly Barros <jotelly@gmail.com>
2018-08-17 13:35:38 -0300

    Use Class POO other example

--- main.cpp
+++ main.cpp
@@ -1,42 +1,35 @@
 // C++ program to demostrate Using Class POO
 #include <iostream>
+#include <typeinfo>
 
-//struct DateStruct
-//{
-//    int year;
-//    int month;
-//    int day;
-
-//    // Function for printing the values
-//    void print(std::string nameDay){
-//        std::cout << nameDay << " is: " << year << "/" << month << "/" << day << std::endl;
-//    }
-//};
-
-class DateClass
+class IntPair
 {
 public:
-    int m_year;
-    int m_month;
-    int m_day;
+    int value_1;
+    int value_2;
 
-    // Print employee information to the screen
-    void print(std::string nameDay){
-        std::cout << nameDay << " is: " << m_year << "/" << m_month << "/" << m_day << std::endl;
+    void set_pair(int v_p1, int v_p2)
+    {
+        value_1 = v_p1;
+        value_2 = v_p2;
+    }
+
+    void print()
+    {
+        std::cout << "Values: [" << value_1 << " : " <<value_2 << "]" << std::endl;
     }
 };
 
+
 int main()
 {
-    // Initialization of struct
-    DateClass yesteday {2018,8,16};
-    DateClass today {2018,8,17};
-    DateClass tomorrow {2018,8,18};
 
-    //today.m_day = 5; // use member selection operator to select a member of the struct
-    yesteday.print("Yesterday");
-    today.print("Today");
-    tomorrow.print("Tomorrow");
+    IntPair p1, p2;
+    p1.set_pair(1,2);
+    p2.set_pair(3,4);
+
+    p1.print();
+    p2.print();
 
     return 0;
 }
