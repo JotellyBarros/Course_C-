1046fffad3bedf85274ace8897119780bcbaf358
Jotelly Barros <jotelly@gmail.com>
2018-08-17 13:45:23 -0300

    Use Class POO whit constructor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// C++ program to demostrate Using Class POO
+// C++ program to demostrate Class POO using constructor
 #include <iostream>
 #include <typeinfo>
 
@@ -7,6 +7,14 @@ class IntPair
 public:
     int value_1;
     int value_2;
+
+    // Default Constructor
+    IntPair()
+    {
+        value_1 = 0;
+        value_2 = 0;
+        std::cout << "Default Constructor called" << std::endl;
+    }
 
     void set_pair(int v_p1, int v_p2)
     {
@@ -25,6 +33,9 @@ int main()
 {
 
     IntPair p1, p2;
+    // whit constructor
+    p1.print();
+
     p1.set_pair(1,2);
     p2.set_pair(3,4);
 
